c0098655c428917233376c777698ebe9912f9e8d
Yair_Blad <yair.dev.py@gmail.com>
2025-08-06 10:42:00 -0600

    se agregan las tablas y la conexion a la bd

--- index.php
+++ index.php
@@ -6,34 +6,65 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primer crud en PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-
+    <script src="https://kit.fontawesome.com/646ac4fad6.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <form>
-        <fieldset disabled>
-            <legend>Disabled fieldset example</legend>
+    <div class="containder-fluid row">
+        <form class="col-4">
+            <h3 class="text-center p-3">Registro de personas</h3>
             <div class="mb-3">
-                <label for="disabledTextInput" class="form-label">Disabled input</label>
-                <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+                <label for="exampleInputEmail1" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="nombre">
             </div>
             <div class="mb-3">
-                <label for="disabledSelect" class="form-label">Disabled select menu</label>
-                <select id="disabledSelect" class="form-select">
-                    <option>Disabled select</option>
-                </select>
+                <label for="exampleInputEmail1" class="form-label">Apellido</label>
+                <input type="text" class="form-control" name="apellido">
             </div>
             <div class="mb-3">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck" disabled>
-                    <label class="form-check-label" for="disabledFieldsetCheck">
-                        Can’t check this
-                    </label>
-                </div>
+                <label for="exampleInputEmail1" class="form-label">DNI</label>
+                <input type="text" class="form-control" name="DNI">
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </fieldset>
-    </form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Fecha de Nacimiento</label>
+                <input type="date" class="form-control" name="fecha">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Correo</label>
+                <input type="text" class="form-control" name="correo">
+            </div>
+            <button type="submit" class="btn btn-primary" name="btnregistrar" value="ok">Registrar</button>
+        </form>
+        <div class="col-8 p-4">
+            <table class="table">
+                <thead class="bg-info">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombres</th>
+                        <th scope="col">Apellidos</th>
+                        <th scope="col">DNI</th>
+                        <th scope="col">Fecha de Nacmiento</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>
+                            <a href="" class="btn btn-small btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="" class="btn btn-small btn-danger"><i class="fa-solid fa-trash"></i></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 
 
---
+++ modelo/conexion.php
@@ -0,0 +1,4 @@
+<?php
+    $conexion = new mysqli("localhost", "root", "", "prueba_php");
+    $conexion->set_charset("utf8")
+?>
