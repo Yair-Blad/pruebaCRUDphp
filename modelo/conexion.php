3b6229aaba50d07aa0972ee0dfe19aaca9df3a53
Yair_Blad <yair.dev.py@gmail.com>
2025-08-07 20:56:54 -0600

    Se agrega la conexion a la BD y tambien se visualizan los datos en la tabla.

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,4 +1,4 @@
 <?php
-    $conexion = new mysqli("localhost", "root", "", "prueba_php");
-    $conexion->set_charset("utf8")
+    $conexion = new mysqli("localhost", "root", "", "prueba_php", "");
+    $conexion -> set_charset("utf8");
 ?>
